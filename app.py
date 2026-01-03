3f33a97a856a970fffdb8cf6b77fab214dbf34ea
sb3136700-sketch <sb3136700@gmail.com>
2026-01-03 13:34:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,6 +105,7 @@ if reset_btn:
 
 # ---------------- EXTRA INFO ----------------
 st.markdown("<hr>", unsafe_allow_html=True)
-st.info("⚠️ This application is designed for academic demonstration only.")
+#st.info("⚠️ This application is designed for academic demonstration only.")
+
 
 
