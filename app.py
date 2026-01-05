5577638729d55b153342548baf497c166503db70
sb3136700-sketch <sb3136700@gmail.com>
2026-01-05 11:44:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ if predict_btn:
     # ---- Diabetes Check ----
     if glucose >= 140 or hba1c >= 6.5:
         st.error("⚠️ Blood Glucose: HIGH RISK")
-    elif glucose >= 140 and hba1c = 7 or hba1c <=8:
+    elif glucose >= 140 and hba1c == 7 or hba1c <=8:
         st.error("✅ Blood Glucose: NORMAL")
     elif 100 <= glucose < 140:
         st.warning("⚠️ Blood Glucose: PREDIABETES")
@@ -126,6 +126,7 @@ if reset_btn:
 # ---------------- EXTRA INFO ----------------
 st.markdown("<hr>", unsafe_allow_html=True)
 #st.info("⚠️ This application is designed for academic demonstration only.")
+
 
 
 
